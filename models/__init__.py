ea610e218aa314092ad2dcc0881d6b548ca29237
Esra <eisra3000ese@gmail.com>
2024-03-25 06:07:41 +0300

    init

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,10 @@
-#!/usr/bin/python3
-"""__init__ magic method for models directory"""
-from models.engine.file_storage import FileStorage
+# -*- coding: utf-8 -*-
 
+from . import base_model, amenity, city, place, review, state
+from . import user
+from .engine.file_storage import FileStorage
+#from tests.test_models.test_base_model import *
 
 storage = FileStorage()
 storage.reload()
+__all__ = ['BaseModel', 'User', 'FileStorage']
--- models/base_model.py
+++ models/base_model.py
@@ -9,32 +9,35 @@ from datetime import datetime
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
-        if kwargs is None:
-            self.id =str(uuid.uuid4())
-            self.created_at = datetime.now()        
-            self.updated_at= datetime.now()
-        else:
-            for key ,val in kwargs.items():
+        if kwargs:
+            for key, val in kwargs.items():
                 if key == "__class__":
-                    pass
-                elif key =="created_at" or key =="updated_at":
-                    dt=datetime.fromisoformat(val)
-                    setattr(self , key, dt)
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.fromisoformat(val))
                 else:
                     setattr(self, key, val)
+        else:
+            self.id = str(uuid4())
+            self.created_at = self.updated_at = datetime.now()
+            models.storage.new(self)
+
     def __str__(self):
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+
     def save(self):
         """updates the public instance attribute updated_at
-        with the current datetime"""
-        self.updated_at=datetime.now()
-        storge=FileStorge()
-        storge.save(self)
+        with the current datetime
+        """
+        self.updated_at = datetime.now()
+        models.storage.new(self)
+
     def to_dict(self):
         """ returns a dictionary containing all keys/values
-        of __dict__ of the instance"""
-        dic_t=self.__dict__.copy()
-        dic_t["__class__"]=type(self).__class__.__name__
-        dic_t["created_at"]=self.created_at.isoformat()
-        dic_t["updated_at"]=self.updated_at.isoformat()
+        of __dict__ of the instance
+        """
+        dic_t = self.__dict__.copy()
+        dic_t["__class__"] = type(self).__name__
+        dic_t["created_at"] = self.created_at.isoformat()
+        dic_t["updated_at"] = self.updated_at.isoformat()
         return (dic_t)
